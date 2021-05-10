91021d70f26beccfa8432ef079e285433565d434
linyu-f <1274282051@qq.com>
2021-05-10 09:59:14 +0800

    5/10

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -6,11 +6,20 @@
           <span>用户注册</span>
         </div>
         <div id="registerContentBody">
-            <el-input id="newusername" v-model="newUser.newusername" clearable></el-input>
-            <el-input id="newpassword" v-model="newUser.newpassword" show-password clearable></el-input>
+          <el-input
+            id="newusername"
+            v-model="newUser.newusername"
+            clearable
+          ></el-input>
+          <el-input
+            id="newpassword"
+            v-model="newUser.newpassword"
+            show-password
+            clearable
+          ></el-input>
         </div>
         <div id="registerSubmit">
-            <el-button id="systemSubmit" @click="registerSystem">注册</el-button>
+          <el-button id="systemSubmit" @click="registerSystem">注册</el-button>
         </div>
       </el-card>
     </div>
@@ -18,27 +27,32 @@
 </template>
 
 <script>
-import Axios from 'axios'
+import Axios from "axios";
 export default {
-  name:"register",
-    data(){
-        return {
-            newUser:{
-                newusername:"",
-                newpassword:""
-            }
-        }
+  name: "register",
+  data() {
+    return {
+      newUser: {
+        newusername: "admin",
+        newpassword: "123456",
+      },
+    };
+  },
+  methods: {
+    // CORS
+    registerSystem() {
+      // Axios.get('http://localhost:3000/register',{
+      //     params:this.newUser
+      // }).then(function(response){
+      //   alert(response.data)
+      // })
+      Axios.post("http://localhost:3000/register", {
+        params: this.newUser,
+      }).then(function (response) {
+        alert(response.data);
+      });
     },
-    methods:{
-      // CORS 
-      registerSystem(){
-        Axios.get('http://localhost:3000/register',{
-            params:this.newUser
-        }).then(function(response){
-          alert(response.data)
-        })
-      }
-    }
+  },
 };
 </script>
 
@@ -47,20 +61,20 @@ export default {
   /* background-color: purple; */
   height: 100%;
 }
-#registerContent{
-    width: 400px;
-    margin:0 auto;
-    padding-top: 100px;
+#registerContent {
+  width: 400px;
+  margin: 0 auto;
+  padding-top: 100px;
 }
-#systemSubmit{
-    display: block;
-    margin: 0 auto;
-    background-color: #0a9588;
-    color:white;
-    border-color: #0a9588;
+#systemSubmit {
+  display: block;
+  margin: 0 auto;
+  background-color: #0a9588;
+  color: white;
+  border-color: #0a9588;
 }
-#newpassword{
-  margin-top:5px;
+#newpassword {
+  margin-top: 5px;
   margin-bottom: 8px;
 }
 </style>
